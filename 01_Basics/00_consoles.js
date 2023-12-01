ee56e6f2b817337c2edd0b9298560a19d78b1789
Tanveer <shaikhtanveer5205@gmail.com>
2023-12-02 02:59:31 +0530

    basic 01 complete

---
+++ 01_Basics/00_consoles.js
@@ -0,0 +1,7 @@
+console.log("hello everyone!! lets start our javascript journey");
+console.log("Tanveer");
+console.error("tanveer");
+console.warn("tanveer");
+console.assert("tanveer");
+console.dir("tanveer");
+// console.clear();
